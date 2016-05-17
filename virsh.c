cf9c8aac74f23e68a8fb8723a05d9fcb234778d4
Nastya-19 <nastysha_19@mail.ru>
2016-05-17 16:36:33 +0300

    Add files via upload

---
+++ virsh.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	printf("I want to be famous\n");
+	printf("Rich and clever too.\n");
+	printf("I want to be a pop star,\n");
+	printf("What about you?\n\n");
+	printf("I want to be famous,\n");
+	printf("Rich and clever too.\n");
+	printf("I want to be a footballer,\n");
+	printf("What about you?\n\n");
+	printf("I want to be famous,\n");
+	printf("Rich and clever too.\n");
+	printf("I want to be a film star,\n");
+	printf("What about you?\n");
+	
+	system("pause");
+	return 0;
+}
